52d1c7fd4904e8fad3ab06a1414b572727b1ccc7
Rich Mama <postmaster.richmama@gmail.com>
2017-12-20 18:35:35 +0100

    Story box

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -35,6 +35,16 @@
 								<p><strong>Please Note:</strong> All Investment that is below Minimum & More than Maximum will count as donation on DoubleYourBTC System.</p>
                                 <p>DoubleYourBTC is the most guaranteed Bitcoin Doubler, our System only takes 24 hours to double your investment.</p> 
                                 <p>You just need to transfer your desired Bitcoin investment to our Address & wait for 24 hours only. All work is automated once 24 hours is completed;<br> You will get just double Bitcoin on your payout wallet Address. <strong>GUARANTEED</strong>.</p>
+							</div><br>
+							<div class="story-box">
+								<h3 class="text-uppercase">How to Invest On DoubleYourBTC</h3>
+								<p>Investment on DoubleYourBTC is pretty simple. You just need to provide us your Bitcoin payout address and the amount you want to invest, and then just click on "Let's Double It!" button.</p>
+								<p>Then after submitting, an alert will show with our Bitcoin address. Now you just need to transfer your desire investment amount on our Bitcoin address & wait for blockchain confirmation.</p>
+								<p>Before blockchain confirmation, your deposit will display on our site as PENDING. Once it has been confirmed 3 times via blockchain, your deposit will be marked as ACTIVE and you will receive double of your investment automatically within 24 hours! </p><br>
+								
+								<h3 class="text-uppercase">How DoubleYourBTC Works</h3>
+								<p>DoubleYourBTC is specifically designed to predict Bitcoin prices from different Bitcoin Exchanges; we use high frequency auto trading bots to buy/sell Bitcoin on price difference & gain lots of profit!</p>
+								<p>We also sell Bitcoin by converting it in local currency at high rates and then buy back Bitcoin at less rates using other exchanges.</p>
 							</div>
 						</div>
 					</div>
