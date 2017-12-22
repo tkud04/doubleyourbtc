215da35ceb569372d93130998fd8f6d2a3bcdd6c
Rich Mama <postmaster.richmama@gmail.com>
2017-12-22 11:43:17 +0100

    Correction in admin page heading

--- resources/views/admin/page-heading.blade.php
+++ resources/views/admin/page-heading.blade.php
@@ -2,7 +2,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            {{$headerBig}} <small>{{$headerSmall;}} </small>
+                            {{$headerBig}} <small>{{$headerSmall}} </small>
                         </h1>
                         <ol class="breadcrumb">
                             <li class="active">
