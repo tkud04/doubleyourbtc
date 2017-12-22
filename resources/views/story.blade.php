3d9091b07ab20f32a0f1dbfc128ff0525dd39033
Rich Mama <postmaster.richmama@gmail.com>
2017-12-22 14:42:45 +0100

    User friendly

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -15,9 +15,11 @@
                           @include('input-errors', ["errors" => $errors])
                      @endif     
 								<form method="post" action="{{url('double')}}">
-									{{ csrf_field() }}          
-									<input type="text" name="amount" placeholder="How much do you want to invest?" onfocus="this.placeholder = ''" onblur="this.placeholder = 'How much do you want to invest?'" required class="single-input-primary" value="{{old('amount')}}"><br>
-									<input type="text" name="wallet_address" placeholder="Enter your personal wallet address (we will send profits here)" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your personal wallet address (we will send profits here)'" required class="single-input-primary" value="{{old('wallet_address')}}"><br>
+									{{ csrf_field() }}   
+                                    <h4>How much do you want to invest? </h4>       
+									<input type="text" name="amount" placeholder="Amount to invest?" onfocus="this.placeholder = ''" onblur="this.placeholder = 'How much do you want to invest?'" required class="single-input-primary" value="{{old('amount')}}"><br>
+									<h4>Your bitcoin wallet address (we will send profits here)</h4>       
+									<input type="text" name="wallet_address" placeholder="Your wallet address (we will send profits here)" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your personal wallet address (we will send profits here)'" required class="single-input-primary" value="{{old('wallet_address')}}"><br>
 									<button type="submit" class="genric-btn primary radius">Let's double it!</button>
 								</form><br>
 								<p><strong>Please Note:</strong> All Investment that is below Minimum & More than Maximum will count as donation on DoubleYourBTC System.</p>
